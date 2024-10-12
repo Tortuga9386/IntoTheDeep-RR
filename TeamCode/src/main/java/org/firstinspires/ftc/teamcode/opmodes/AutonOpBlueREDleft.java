1e015981820bd502fd1b2c442b667909648d467c
mchilek <mchilek@gmail.com>
2024-10-12 16:21:40 -0500

    made a left auto and modded it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonOpBlueREDleft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutonOpBlueREDleft.java
@@ -9,7 +9,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
 
-@Autonomous(name="2) RIGHT", group = "auto")
+@Autonomous(name="1) LEFT SPECIMEN", group = "auto")
 public final class AutonOpBlueREDleft extends LinearOpMode {
 
     double SOUTH = Math.PI;
@@ -19,7 +19,7 @@ public final class AutonOpBlueREDleft extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Pose2d beginPose = new Pose2d(16.5, -65, WEST);
+        Pose2d beginPose = new Pose2d(-29.5, -65, WEST);
         if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
             MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
 
@@ -27,24 +27,24 @@ public final class AutonOpBlueREDleft extends LinearOpMode {
 
             Actions.runBlocking(
                     drive.actionBuilder(beginPose)
-                            .splineToConstantHeading(new Vector2d(4, -29), WEST) //spline out to the sub
-                            .waitSeconds(2) //back up from the sub
+                            .splineToConstantHeading(new Vector2d(-4, -29), WEST) //spline out to the sub
+                            .waitSeconds(2)
                             .lineToYConstantHeading(-35)//back up from the sub
-                            .splineToConstantHeading(new Vector2d(36,-24 ), WEST)
+                            .splineToConstantHeading(new Vector2d(-47,-44 ), WEST)
+                            .turnTo(EAST)
+                            .waitSeconds(0.01)
+                            .splineToConstantHeading(new Vector2d(-63,-54), EAST)// baskettttt
+                            .waitSeconds(0.01)
+                            .strafeToConstantHeading(new Vector2d(-60,-54))
+                            .turnTo(WEST)
+                            .splineToConstantHeading(new Vector2d(-56,-44),WEST)
+                            .turnTo(EAST)
+                            .waitSeconds(0.01)
+                            .splineToConstantHeading(new Vector2d(-63,-54), EAST)// baskettttt
+                            .waitSeconds(0.01)
+                            .strafeToConstantHeading(new Vector2d(-58, -54))
                             .turnTo(NORTH)
-                            .lineToXConstantHeading(46)//captures first block
-                            .turnTo(EAST)
-                            .splineToConstantHeading(new Vector2d(48, -60), EAST) // pushes the the first block
-                            .lineToY(-12)
-                            .strafeToConstantHeading(new Vector2d(54, -12)) //strafe aiming for the second block
-                            .waitSeconds(0.01)
-                            .strafeToConstantHeading(new Vector2d(54,-60)) //pushes the second block into the zone
-                            .lineToY(-12)
-                            .strafeToConstantHeading(new Vector2d(62, -12)) //strafe aiming for the third block
-                            .waitSeconds(0.01)
-                            .turnTo(EAST)
-                            .strafeToConstantHeading(new Vector2d(62,-60)) //pushes the third block into the zone
-                            //.strafeToConstantHeading(new Vector2d(66,-12))
+                            .splineToConstantHeading(new Vector2d(-24,-6),NORTH)//level 1 ascent
                             .build());
         } else {
             throw new RuntimeException();
