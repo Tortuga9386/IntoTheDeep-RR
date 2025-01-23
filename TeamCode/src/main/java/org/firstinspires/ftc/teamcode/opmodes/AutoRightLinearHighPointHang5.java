6d3f14830f3c8a6a332822df51ea6eaafd0b8ddf
Nick <eanderobotics@gmail.com>
2025-01-23 16:55:07 -0600

    testing github

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoRightLinearHighPointHang5.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoRightLinearHighPointHang5.java
@@ -2,26 +2,20 @@ package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.AngularVelConstraint;
-import com.acmerobotics.roadrunner.MinVelConstraint;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
-import com.acmerobotics.roadrunner.TranslationalVelConstraint;
 import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.VelConstraint;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.MecanumDrive;
 
-import java.util.Arrays;
-
 @Config
-@Autonomous(name = "AUTO_RIGHT_LINEAR_5_SPECIMEN_PATH", group = "Autonomous")
-public class AutoRightLinearHighPointHang5_path extends LinearOpMode {
+@Autonomous(name = "AUTO_RIGHT_LINEAR_5_SPECIMEN", group = "Autonomous")
+public class AutoRightLinearHighPointHang5 extends LinearOpMode {
 
     double SOUTH = Math.PI;
     double EAST = 3 * Math.PI / 2;
